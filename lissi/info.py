001142378306be14a2a3037bdd333de61dbed5af
Marco Genasci <fedeliallalinea@gmail.com>
2021-09-22 20:22:55 +0200

    Version 0.0.4
    - add gentoo mailing list plugin

--- lissi/info.py
+++ lissi/info.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __homepage__ = 'https://github.com/fedeliallalinea/lissi'
 __bugto__ = 'https://github.com/fedeliallalinea/lissi/issues'
 __authors__ = 'Gentoo Linux Forums Team'
