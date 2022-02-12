df7c19b4e2ed5a01590b36a4dcf0ee1ae88b5f69
Marco Genasci <fedeliallalinea@gmail.com>
2022-02-12 20:25:38 +0100

    Version 0.0.5
    Fix beautifulsoup4 dependency name

--- lissi/info.py
+++ lissi/info.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __homepage__ = 'https://github.com/fedeliallalinea/lissi'
 __bugto__ = 'https://github.com/fedeliallalinea/lissi/issues'
 __authors__ = 'Gentoo Linux Forums Team'
